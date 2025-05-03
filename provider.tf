72c1dbe8aca4dade9cb57c0550f60fdba16704f4
Bernard Olatunde Oddiah <bernard.oddiah@gmail.com>
2025-05-03 11:44:48 +0100

    Update provider.tf - 3

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.97.0"
+    }
+  }
+}
+
 provider "aws" {
   # Configuration options
   region = "us-east-1"
